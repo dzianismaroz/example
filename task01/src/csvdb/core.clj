52a61c5becce1d4d8c8fa67d6a67f9766a7f9914
dzianismaroz <kapitan.frost@gmail.com>
2018-09-29 17:15:46 +0300

    fix of clojure version 1.8.0 artifact

--- task01/src/csvdb/core.clj
+++ task01/src/csvdb/core.clj
@@ -149,3 +149,4 @@
 ;; => ({:subject "Math", :subject_id 1, :surname "Ivanov", :year 1998, :student_id 1, :id 1} {:subject "Math", :subject_id 1, :surname "Petrov", :year 1997, :student_id 2, :id 2})
 
 ;; end of file
+(println "marina ! privet")
